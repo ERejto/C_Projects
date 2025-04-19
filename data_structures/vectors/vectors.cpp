4fda41a0cb4b9ead82e2f812a4deafe922abc057
ERejto <eli.rejto@gmail.com>
2025-04-18 20:37:24 -0700

    basic vector implementation

--- data_structures/vectors/vectors.cpp
+++ data_structures/vectors/vectors.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
-using std::cout;
+using namespace std;
 
+template <typename T>
 class vector {
+        T* data;
+        size_t size = 0;
+        size_t capacity = 10;
     public:
-        vector(int values) {
-            
+        vector() {
+            data = (T*)malloc(capacity);
         }
         ~vector(){
+            free(data);
+        }
+        void push_back(T value) {
+            if (size > capacity){
+                capacity = capacity * 2;
+                data = (T*)realloc(data, capacity);
+            }
+            *(data+size) = value;
+            size += 1;
+        }
+        T& operator [](size_t index) {
+            return *(data+index); 
+        }
+        size_t get_size(){
+            return size;            
         }
 };
 
-
-//access @ index
-//remove last value
-
-
 int main() {
-    vector vec(13);
+    vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(10);
+    int size = vec.get_size();
+    cout << vec[1] << "\n";
+    cout << size << "\n";
+    vec.push_back(1);
+    vec.push_back(2);
+    size = vec.get_size();
+    cout << size << "\n";
     return 0;
 }
