feef10da0e5797624ca0fa95c0a38caf46182d5b
ERejto <eli.rejto@gmail.com>
2025-04-18 23:57:28 -0700

    adding += and initializer list support

--- data_structures/vectors/vectors.cpp
+++ data_structures/vectors/vectors.cpp
@@ -1,44 +1,70 @@
 #include <iostream>
-using namespace std;
-
+#include <initializer_list>
+#include <cstdlib>
+using std::cout;
+using std::cin;
 template <typename T>
 class vector {
+    private:
         T* data;
         size_t size = 0;
         size_t capacity = 10;
+        size_t data_size = sizeof(T);
     public:
         vector() {
-            data = (T*)malloc(capacity);
+            data = (T*)malloc(capacity*data_size);
+        }
+        vector(size_t sz) {
+            size = sz;
+            capacity=sz;
+            data = (T*)malloc(size*data_size);
+        }
+        vector(std::initializer_list<T> list) {
+            data = (T*)malloc(capacity*data_size);
+            for(T value : list){
+                push_back(value);
+            }
         }
         ~vector(){
             free(data);
         }
         void push_back(T value) {
-            if (size > capacity){
-                capacity = capacity * 2;
-                data = (T*)realloc(data, capacity);
+            if (size >= capacity){
+                capacity *=  2;
+                data = (T*)realloc(data, capacity*data_size);
             }
-            *(data+size) = value;
-            size += 1;
+            data[size++] = value;
         }
         T& operator [](size_t index) {
-            return *(data+index); 
+            return data[index]; 
+        }
+        void operator +=(T value){
+            push_back(value);
+        }
+        void operator +=(std::initializer_list<T> list){
+            for(T value : list) {
+                push_back(value);
+            }
         }
         size_t get_size(){
             return size;            
         }
+        T& begin(){
+            return data[0];
+        }
+        T& end(){
+            return data[size-1];
+        }
+        void print() {
+            cout << "[ ";
+            for(int i=0; i<size; i++){
+                cout << data[i] << " ";
+            }
+            cout << "]" << "\n";
+        } 
 };
 
 int main() {
-    vector<int> vec;
-    vec.push_back(5);
-    vec.push_back(10);
-    int size = vec.get_size();
-    cout << vec[1] << "\n";
-    cout << size << "\n";
-    vec.push_back(1);
-    vec.push_back(2);
-    size = vec.get_size();
-    cout << size << "\n";
+    vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
     return 0;
 }
